1334f5c66730625937744e7576a324ad3cdbed5c
daniel <dab180719@gmail.com>
2023-06-12 18:48:11 -0600

    Asigne las variables

---
+++ app.js
@@ -0,0 +1,9 @@
+/* Asignacion de variables */
+
+const imagenes = document.querySelectorAll('.contenedorImagenes .Imagenes')
+let contPrevisualizado = document.querySelector('.contImgZoom')
+let imgPresentada = contPrevisualizado.querySelector('img')
+let descripcion = contPrevisualizado.querySelector('.parrafo')
+let btnClose = contPrevisualizado.querySelector('.btnCerrar')
+let contFig = document.querySelectorAll('.contenedorImagenes .Figuras')
+
